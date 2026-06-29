42ba32a58f42206cc5e53a1add4726239193e1ac
IsaiasEGarrido <isaiasegarrido@gmail.com>
2026-06-29 17:32:48 -0600

    feat: improve readability by changing g to game in find loop

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const games = require('./games');
 app.get('/api/games', (req, res) => {res.json(games)});
 
 app.get('/api/games/:id', (req, res) => {
-    const gameId = parseInt(req.params.id, 10);
-    const foundGame = games.find(g => g.id === gameId);
+    const gameId = parseInt(req.params.id);
+    const foundGame = games.find(game => game.id === gameId);
     res.json(foundGame);
-});
+} );
 
 app.listen(3000, () => {console.log('Server is running on port 3000')});
